b676d1c51a5ea975f7a835a8ba003cb01cf7c332
heolazz <heolazz.gmail.com>
2026-03-16 13:49:48 +0700

    feat(data): update events with real coffee shop assets

--- constants.tsx
+++ constants.tsx
@@ -4,45 +4,45 @@ export const MOCK_EVENTS: Event[] = [
   {
     id: '1',
     title: 'Sabtu Pagi Espresso & Coding',
-    date: 'SAB, 18 NOV',
+    date: 'SAB, 22 MAR',
     time: '10:00 WIB',
-    location: 'Filosofi Kopi, Jakarta Selatan',
+    location: 'Titik Temu Coffee, Senopati',
     category: 'Teknologi',
     attendees: 12,
-    imageUrl: 'https://picsum.photos/800/600?random=1',
-    host: 'Sarah Jenkins'
+    imageUrl: '/coffee-shop/titik-temu-coffee.jpg',
+    host: 'Aris Setiawan'
   },
   {
     id: '2',
     title: 'Workshop Latte Art Pemula',
-    date: 'MIN, 19 NOV',
+    date: 'MIN, 23 MAR',
     time: '14:00 WIB',
-    location: 'Anomali Coffee, Senopati',
+    location: 'Jacob Koffie Huis, Depok',
     category: 'Workshop',
     attendees: 25,
-    imageUrl: 'https://picsum.photos/800/600?random=2',
-    host: 'Marco Rossi'
+    imageUrl: '/coffee-shop/jacob-koffie-huis.jpg',
+    host: 'Dinda Kirana'
   },
   {
     id: '3',
     title: 'Networking Freelancer Kreatif',
-    date: 'SEN, 20 NOV',
+    date: 'SEN, 24 MAR',
     time: '09:00 WIB',
-    location: 'Workroom Coffee, Cikini',
+    location: 'Kopi Kotak, Margonda',
     category: 'Bisnis',
     attendees: 8,
-    imageUrl: 'https://picsum.photos/800/600?random=3',
-    host: 'Alex Chen'
+    imageUrl: '/coffee-shop/kopi-kotak-margonda.webp',
+    host: 'Raka Pratama'
   },
   {
     id: '4',
     title: 'Klub Buku: Klasik & Cappuccino',
-    date: 'RAB, 22 NOV',
+    date: 'RAB, 26 MAR',
     time: '18:30 WIB',
-    location: 'Tuku Kopi, Cipete',
+    location: 'Kylau Common Space, Jakarta',
     category: 'Budaya',
     attendees: 15,
-    imageUrl: 'https://picsum.photos/800/600?random=4',
-    host: 'Emily Page'
+    imageUrl: '/coffee-shop/kylau-common-space.jpg',
+    host: 'Siska Amelia'
   }
 ];
